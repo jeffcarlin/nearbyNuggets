4c9d3b48356872aa7b1bde97c315c4ad58f36584
Jeff Carlin <jcarlin@lsst.org>
2022-12-11 14:00:03 -0800

    Change find10sigma to findNsigma

--- nearbyNuggets/inputCatalogs/characterize.py
+++ nearbyNuggets/inputCatalogs/characterize.py
@@ -2,11 +2,11 @@ import numpy as np
 import scipy.stats as stats
 
 
-def find10sigma(mags, errors, magbinsize=0.02, minmag=15.0, maxmag=29.0):
+def findNsigma(mags, errors, nsig=10.0, magbinsize=0.02, minmag=15.0, maxmag=29.0):
     # Note: errors can be either mag. errors or fractional flux errors
     errbins = np.arange(minmag, maxmag, magbinsize)
     errbin_medians = stats.binned_statistic(mags, errors, statistic='median', bins=errbins)
-    minloc_err = np.nanargmin(np.abs(errbin_medians[0]-0.1))
+    minloc_err = np.nanargmin(np.abs(errbin_medians[0]-(1/nsig)))
     return errbin_medians[1][minloc_err]
 
 
