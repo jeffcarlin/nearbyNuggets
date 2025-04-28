de866917a99414ad036bd2568187477ec1efd914
Jeff Carlin <jeffreylcarlin@gmail.com>
2025-04-27 18:46:25 -0700

    Add butler version of plotcand6

--- nearbyNuggets/analysis/diagnosticPlots.py
+++ nearbyNuggets/analysis/diagnosticPlots.py
@@ -9,6 +9,58 @@ from nearbyNuggets.toolbox import img_scale
 from astropy.wcs import WCS
 # from astropy.convolution import convolve, Gaussian2DKernel
 from nearbyNuggets.toolbox.utils import lf_powerlaw
+from lsst.daf.butler import Butler
+import lsst.geom as geom
+
+
+def cutout_coadd(butler, ra, dec, band='r', datasetType='deepCoadd_calexp',
+                 skymap=None, cutoutSideLength=51, **kwargs):
+    """
+    Produce a cutout from a coadd at the given ra, dec position.
+
+    Adapted from DC2 tutorial notebook by Michael Wood-Vasey.
+
+    Parameters
+    ----------
+    butler: lsst.daf.persistence.Butler
+        Helper object providing access to a data repository
+    ra: float
+        Right ascension of the center of the cutout, in degrees
+    dec: float
+        Declination of the center of the cutout, in degrees
+    band: string
+        Filter of the image to load
+    datasetType: string ['deepCoadd']
+        Which type of coadd to load.  Doesn't support 'calexp'
+    skymap: lsst.afw.skyMap.SkyMap [optional]
+        Pass in to avoid the Butler read.  Useful if you have lots of them.
+    cutoutSideLength: float [optional]
+        Size of the cutout region in pixels.
+
+    Returns
+    -------
+    MaskedImage
+    """
+    radec = geom.SpherePoint(ra, dec, geom.degrees)
+    cutoutSize = geom.ExtentI(cutoutSideLength, cutoutSideLength)
+
+    if skymap is None:
+        skymap = butler.get("skyMap")
+
+    # Look up the tract, patch for the RA, Dec
+    tractInfo = skymap.findTract(radec)
+    patchInfo = tractInfo.findPatch(radec)
+    xy = geom.PointI(tractInfo.getWcs().skyToPixel(radec))
+    bbox = geom.BoxI(xy - cutoutSize // 2, cutoutSize)
+    patch = tractInfo.getSequentialPatchIndex(patchInfo)
+
+    coaddId = {'tract': tractInfo.getId(), 'patch': patch, 'band': band}
+    parameters = {'bbox': bbox}
+
+    cutout_image = butler.get(datasetType, parameters=parameters,
+                              dataId=coaddId)
+
+    return cutout_image, bbox
 
 
 def plotcand6(sc_inp, cat, i, nsig,
@@ -1172,6 +1224,334 @@ def plotcand6_gimli(sc_inp, cat, i, nsig,
         plt.show()
 
     plt.close()
+
+
+
+def plotcand6_butler(butler, sc_inp, cat, nsig,
+                     comp_flag, cmdsel_flag,
+                     star_flag,
+                     binsize=1.5, recalc_cen=False, savefig=False,
+                     name_append='', overlay_pts=False):
+    """ 6 panel diagnostic figure -- CMD, spatial plot, density profile,
+        color image, smoothed images in g and i
+
+    Parameters
+    ----------
+    butler :
+        Butler containing the images and catalogs
+    sc_inp : `astropy.skycoord`
+        input central coordinates of the candidate's position
+    ###sc_all : `astropy.skycoord`
+        ###input coordinates of all objects
+    cat : `Astropy Table`
+        input catalog of all data
+    ### gi_mags : ``
+        ### arrays of g and i-band magnitudes ([g_array, i_array])
+    comp_flag : `` "cen"
+        array of boolean flag values for the comparison population
+        (e.g., "cen" for the central regions of the host)
+    cmdsel_flag : `` "rgbbox_blue"
+        array of boolean flag values to select the sample of interest
+        (e.g., RGB stars)
+    ### cand_flag_cmd : `` "dwarf_msk_cmd"
+        ### array of boolean flag values to select the candidate of interest
+        ### (e.g., a candidate dwarf galaxy) for CMD plotting
+    ### cand_flag : `` "dwarf_msk"
+        ### array of boolean flag values to select the candidate of interest
+        ### (e.g., a candidate dwarf galaxy)
+    star_flag : `` "isstar"
+        array of boolean flag values to select point sources (i.e., stars)
+    binsize : `astropy.quantity`
+        size of bins in arcminutes
+
+    nsig : `float`
+        significance (in # of sigma excess above background) of overdensity
+    recalc_cen : `bool`
+
+    savefig : `bool`
+
+    name_append : `str`
+
+    overlay_pts : `bool`
+        whether to overlay detections on the images
+    """
+
+# To do:
+# add RGB box corners as input params
+
+    sc_all = SkyCoord(ra=cat['coord_ra']*u.degree, dec=cat['coord_dec']*u.degree, frame='icrs')
+    ra = sc_all.ra.value
+    dec = sc_all.dec.value
+    gmag = cat['g0']
+    imag = cat['i0']
+    gi = gmag-imag
+
+    params = {
+        'axes.labelsize': 14,
+        'font.size': 14,
+        'legend.fontsize': 12,
+        #   'xtick.labelsize': 16,
+        'xtick.major.width': 3,
+        'xtick.minor.width': 2,
+        'xtick.major.size': 8,
+        'xtick.minor.size': 5,
+        'xtick.direction': 'in',
+        'xtick.top': True,
+        'lines.linewidth': 3,
+        'axes.linewidth': 3,
+        'axes.labelweight': 3,
+        'axes.titleweight': 3,
+        'ytick.major.width': 3,
+        'ytick.minor.width': 2,
+        'ytick.major.size': 8,
+        'ytick.minor.size': 5,
+        'ytick.direction': 'in',
+        'ytick.right': True,
+        #   'ytick.labelsize': 20,
+        'text.usetex': True,
+        'text.latex.preamble': r'\boldmath',
+        'figure.facecolor': 'White',
+        'figure.figsize': [14, 18]
+    }
+
+    plt.rcParams.update(params)
+
+    binsize = binsize*u.arcmin
+
+    # Recalculate the center based on RGB stars, if flag is set:
+    if recalc_cen:
+        dwarf_msk_tmp = sc_all.separation(sc_inp) < (1.5*binsize)
+        med_ra = np.median(ra[dwarf_msk_tmp & star_flag & cmdsel_flag])
+        med_dec = np.median(dec[dwarf_msk_tmp & star_flag & cmdsel_flag])
+        sc_bin = SkyCoord(med_ra*u.deg, med_dec*u.deg)
+    else:
+        sc_bin = sc_inp.copy()
+
+    # For testing:
+    print('sc_bin: ', sc_bin)
+    print('sc_inp: ', sc_inp)
+
+    cand_flag = sc_all.separation(sc_bin) < (1.5*binsize)
+    cand_flag_cmd = sc_all.separation(sc_bin) < (1.0*binsize)
+    cand_flag_bg_cmd = (sc_all.separation(sc_bin) < (2.0*binsize)) &\
+                       (sc_all.separation(sc_bin) > np.sqrt(3)*binsize)
+
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # Color-magnitude diagram:
+    plt.subplot(321)
+    plt.scatter(gi[comp_flag & star_flag], imag[comp_flag & star_flag], s=1,
+                c='Gray', alpha=.5, label='comparison')
+    plt.scatter(gi[cand_flag_cmd & star_flag], imag[cand_flag_cmd & star_flag], s=40,
+                color='DodgerBlue', label='candidate overdensity')
+    plt.scatter(gi[cand_flag_cmd & star_flag & cmdsel_flag],
+                imag[cand_flag_cmd & star_flag & cmdsel_flag], s=40,
+                color='Red', label='RGB')
+
+    # To do:
+    # plt.plot(xcorners_blue, ycorners_blue, color='Gray')
+
+    plt.legend(loc='upper left')
+    plt.xlim(-1, 3)
+    plt.ylim(26.8, 19.8)
+    plt.xlabel(r'$(g-i)_0$')
+    plt.ylabel(r'$i_0$')
+    plt.minorticks_on()
+
+    # Print the original bin center on the CMD:
+    ra_str = str('{:7.3f}'.format(sc_inp.ra.value))
+    dec_str = str('{:7.3f}'.format(sc_inp.dec.value))
+    nsig_str = str('{:5.1f}'.format(nsig))
+    plt.title(r', (RA, Dec)$_0$=('+ra_str+','+dec_str+'), Nsig='+nsig_str)
+
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # Spatial plot:
+    plt.subplot(322)
+    plt.plot(ra[cand_flag & ~star_flag], dec[cand_flag & ~star_flag], '.',
+             ms=4, color='Gray', alpha=0.3, label='not stars')
+    plt.plot(ra[cand_flag & star_flag], dec[cand_flag & star_flag], '.',
+             ms=10, color='DodgerBlue', label='stars')
+    plt.plot(ra[cand_flag & star_flag & cmdsel_flag], dec[cand_flag & star_flag & cmdsel_flag],
+             '.', ms=10, color='Red', label='RGB stars')
+    plt.xlabel('RA (deg)')
+    plt.ylabel('DEC (deg)')
+    plt.legend(loc='upper left')
+    plt.gca().invert_xaxis()
+    plt.locator_params(axis='x', nbins=4)
+    plt.locator_params(axis='y', nbins=5)
+    plt.minorticks_on()
+
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # 1D radial density plot
+    nradbins = 10
+    radbinsize = 2*binsize/nradbins
+    radbins = np.linspace(0, binsize, nradbins+1)
+    radbin_cens = (radbins[1:] + radbins[:-1]) / 2.0
+    rad_density = np.zeros(nradbins)
+    for irad in range(nradbins):
+        area = 2*np.pi*(radbins[irad+1].value**2 - radbins[irad].value**2)
+        annulus_mask = (sc_all.separation(sc_bin) >= radbins[irad]) &\
+                       (sc_all.separation(sc_bin) < radbins[irad+1])
+        rad_density[irad] = len(ra[annulus_mask & star_flag & cmdsel_flag]) / area
+
+    plt.subplot(323)
+    plt.plot(radbin_cens, rad_density, '-.')
+    plt.plot(radbin_cens, rad_density, 'ko')
+    plt.minorticks_on()
+
+    # if recalc_cen:
+    ra_str = str('{:7.3f}'.format(sc_bin.ra.value))
+    dec_str = str('{:7.3f}'.format(sc_bin.dec.value))
+    nsig_str = str('{:5.1f}'.format(nsig))
+    plt.title(r', (RA, Dec)$_0$=('+ra_str+','+dec_str+'), Nsig='+nsig_str)
+
+    plt.xlabel('Distance from center [arcmin]')
+    plt.ylabel(r'Number of stars per arcsec$^2$')
+
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # Color image:
+    plt.subplot(324)
+    censtar = np.where(sc_all.separation(sc_bin) == np.min(sc_all.separation(sc_bin)))
+
+    # gcoadd_refs = butler.query_datasets('deepCoadd_calexp',
+    #                                     where="patch.region OVERLAPS POINT(ra, dec) AND band='g'",
+    #                                     bind={'ra': censtar.ra.value, 'dec': censtar.dec.value})
+    # icoadd_refs = butler.query_datasets('deepCoadd_calexp',
+    #                                     where="patch.region OVERLAPS POINT(ra, dec) AND band='i'",
+    #                                     bind={'ra': censtar.ra.value, 'dec': censtar.dec.value})
+
+    # tract = gcoadd_refs[0].dataId['tract']
+    # patch = gcoadd_refs[0].dataId['patch']
+
+    pixscale = 0.168 # arcsec per pixel; hard-coded for HSC
+    xy_pix_size = int(np.floor(2.0*binsize.to(u.arcsec).value/pixscale))
+
+    cutout_g, bbox_g = cutout_coadd(butler, censtar.ra.value, censtar.dec.value, band='g',
+                                    datasetType='deepCoadd_calexp', cutoutSideLength=xy_pix_size)
+    cutout_i, bbox_i = cutout_coadd(butler, censtar.ra.value, censtar.dec.value, band='i',
+                                    datasetType='deepCoadd_calexp', cutoutSideLength=xy_pix_size)
+    cutout_r = cutout_g.clone()
+    cutout_r.image.array = (cutout_g.image.array + cutout_i.image.array)/2.0
+
+    zscale = ZScaleInterval()
+    vmin_g, vmax_g = zscale.get_limits(cutout_g.image.array)
+    vmin_r, vmax_r = zscale.get_limits(cutout_r.image.array)
+    vmin_i, vmax_i = zscale.get_limits(cutout_i.image.array)
+
+    img = np.zeros((cutout_i.image.array.shape[0], cutout_i.image.array.shape[1], 3), dtype=float)
+    img[:, :, 0] = img_scale.linear(cutout_i.image.array, scale_min=0.1*vmin_i, scale_max=0.6*vmax_i)
+    img[:, :, 1] = img_scale.linear(cutout_r.image.array, scale_min=0.1*vmin_r, scale_max=0.9*vmax_r)
+    img[:, :, 2] = img_scale.linear(cutout_g.image.array, scale_min=0.1*vmin_g, scale_max=0.75*vmax_g)
+
+    # # Create RA, Dec ticks, labels:
+    # nticks = 4
+    # xtick_locs = np.arange(0, xy_pix_size, xy_pix_size/nticks) + xy_pix_size/(2*nticks)
+    # ytick_locs = np.arange(0, xy_pix_size, xy_pix_size/nticks) + xy_pix_size/(2*nticks)
+
+    # tick_locs_tmp = []
+    # for loc in xtick_locs:
+    #     tick_locs_tmp.append(cutout_g.to_original_position((loc, 0)))
+
+    # xtick_locs_orig = []
+    # for loc in tick_locs_tmp:
+    #     xtick_locs_orig.append(loc[0])
+
+    # tick_locs_tmp = []
+    # for loc in ytick_locs:
+    #     tick_locs_tmp.append(cutout_g.to_original_position((0, loc)))
+
+    # ytick_locs_orig = []
+    # for loc in tick_locs_tmp:
+    #     ytick_locs_orig.append(loc[1])
+
+    # xtick_lbls = []
+    # ytick_lbls = []
+    # for xt, yt in zip(xtick_locs_orig, ytick_locs_orig):
+    #     temp = w.all_pix2world([[xt, yt]], 0)
+    #     xtick_lbls.append(str(np.round(temp[0][0], 2)))
+    #     ytick_lbls.append(str(np.round(temp[0][1], 2)))
+
+    fig = plt.gca()
+    # fig.axes.xaxis.set_ticks(xtick_locs, labels=xtick_lbls)
+    # fig.axes.yaxis.set_ticks(ytick_locs, labels=ytick_lbls)
+    # fig.axes.tick_params(direction='out')
+    fig.axes.get_xaxis().set_visible(False)
+    fig.axes.get_yaxis().set_visible(False)
+
+    linelength_arcsec = 10.0
+    linelength_pix = linelength_arcsec/pixscale
+    fig.hlines(60, 70, 70+linelength_pix, color='White')
+    fig.text(30, 85, '10 arcsec', color='White')
+
+    fig.imshow(img, aspect='equal', origin='lower', extent=[0, xy_pix_size, 0, xy_pix_size])
+
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # Luminosity function:
+    plt.subplot(325)
+
+    lf_binsize = 0.25
+    lf_minmag = 21.25
+    lf_maxmag = 26.5
+    lf_bins = np.arange(lf_minmag, lf_maxmag, lf_binsize)
+    imag_cand_rgb = imag[cand_flag_cmd & star_flag & cmdsel_flag]
+    imag_cand_bg_rgb = imag[cand_flag_bg_cmd & star_flag & cmdsel_flag]
+    imag_cand_all = imag[cand_flag & ~cand_flag_cmd & star_flag]
+
+    lf_mag_all, lf_hist_bins = np.histogram(imag_cand_all, bins=lf_bins)
+    lf_bin_cens = lf_hist_bins + lf_binsize/2.0
+    lf_mag_rgb, lf_hist_bins = np.histogram(imag_cand_rgb, bins=lf_bins)
+    lf_mag_bg_rgb, lf_hist_bins = np.histogram(imag_cand_bg_rgb, bins=lf_bins)
+
+    plt.plot(lf_bin_cens[:-1], np.cumsum(lf_mag_rgb), '-.', color='Red', label='cmd selected')
+    plt.plot(lf_bin_cens[:-1], np.cumsum(lf_mag_rgb), 'ko')
+    plt.plot(lf_bin_cens[:-1], np.cumsum(lf_mag_bg_rgb), '-.', color='Gray', label='background w/ cmd selection')
+    plt.plot(lf_bin_cens[:-1], np.cumsum(lf_mag_bg_rgb), 'ko')
+    plt.legend()
+    plt.minorticks_on()
+    plt.semilogy()
+
+    lf_salpeter = lf_powerlaw(lf_bin_cens, lf_mag_rgb, alpha=2.35)
+    lf_geha = lf_powerlaw(lf_bin_cens, lf_mag_rgb, alpha=1.30)
+
+    plt.ylim(8*10**(-1), 1.15*np.sum(lf_mag_rgb))
+
+    plt.xlabel('imag')
+    plt.ylabel(r'$N_{*, cand} <$ imag')
+
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # Color image:
+    plt.subplot(326)
+
+    fig = plt.gca()
+    # fig.axes.xaxis.set_ticks(xtick_locs, labels=xtick_lbls)
+    # fig.axes.yaxis.set_ticks(ytick_locs, labels=ytick_lbls)
+    # fig.axes.tick_params(direction='out')
+    fig.axes.get_xaxis().set_visible(False)
+    fig.axes.get_yaxis().set_visible(False)
+
+    wcs = cutout_g.wcs
+    x_rgb, y_rgb = wcs.skyToPixelArray(ra[cand_flag & star_flag & cmdsel_flag],
+                                       dec[cand_flag & star_flag & cmdsel_flag],
+                                       degrees=True)
+
+    # x_rgb, y_rgb = w.all_world2pix(ra[cand_flag & star_flag & cmdsel_flag],
+    #                                dec[cand_flag & star_flag & cmdsel_flag], 0)
+
+    if overlay_pts:
+        fig.plot(x_rgb-bbox_g.getBeginX(), y_rgb-bbox_g.getBeginY(), 'ws', ms=10,
+                 markeredgewidth=1, fillstyle='none')
+
+    fig.imshow(img, aspect='equal', origin='lower', extent=[0, xy_pix_size, 0, xy_pix_size])
+
+    if savefig:
+        savefile = 'cand_diagnostic_plots_6panel_'+name_append+'.png'
+        plt.savefig(savefile, dpi=180)
+        print('Saved figure to ', savefile, '. Rename to avoid overwriting.')
+    else:
+        plt.show()
+
+    plt.close()
+
+
 
 '''
     # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
